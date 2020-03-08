184c2d0552e85e8f76fa95037f23c15583c03f17
Stengrat <ygors5659@gmail.com>
2020-03-08 19:15:25 -0300

    inicio da home

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.paginaInicial, name='home')
+
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def paginaInicial(request):
     context = {}
     return render(request, 'home/index.html', context)
+
+
