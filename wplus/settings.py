c37ad5e214e67c6b22ec2de9b64d32fb3d9303b1
Stengrat <ygorsa1998@hotmail.com>
2020-03-12 22:27:35 -0300

    Adicionado configs do mysql

--- wplus/settings.py
+++ wplus/settings.py
@@ -79,11 +79,25 @@ WSGI_APPLICATION = 'wplus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'weekend',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+
     }
+}
+'''
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        }
 }
 
 
