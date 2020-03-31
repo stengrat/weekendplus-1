5b0f9f511ec9e9b5d4fd424edbf66aba67a47df4
Ricardo Taverna <taverna.ricardo@gmail.com>
2020-03-31 17:18:59 -0300

    pagina de filmes atualizada

--- midia/views.py
+++ midia/views.py
@@ -6,6 +6,8 @@ from .models import Midia
 
 def paginaFilmesSeries(request):
     filmes = Midia.objects.filter(categoria = "Filme")
+    series = Midia.objects.filter(categoria = "Serie")
+    ultimos = Midia.objects.all().order_by('-id')[:10]
 
-    context = {'filmes': filmes}
+    context = {'filmes': filmes, 'series': series, 'ultimos':ultimos}
     return render(request, 'midia/pagina_filmes_series.html', context)
