4ecd75eb5b5ea020ec9654009a7dc04125d5d881
Stengrat <ygorsa1998@hotmail.com>
2020-04-23 20:19:54 -0300

    mudado para banco local

--- wplus/settings.py
+++ wplus/settings.py
@@ -84,9 +84,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'weekendplus',
-        'USER': 'weekendplus',
-        'PASSWORD': 'fR3FCAjFyq9eDFRV',
-        'HOST': '144.91.110.253',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
 
     }
