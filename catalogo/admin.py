e0b318d5cb989885bafded5c2a6e67b636979dcb
RicardoTaverna <taverna.ricardo@gmail.com>
2020-05-31 17:46:40 -0300

    modificações no db

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Filmes, Series, FavoritosFilmes
+from .models import Filmes, Series, FavoritosFilmes, SerieEpisodio
 
 admin.site.register(Filmes)
 admin.site.register(Series)
 admin.site.register(FavoritosFilmes)
+admin.site.register(SerieEpisodio)
